c82618a336da5a97a0d22cddf8af1db6b796ed77
Googlingprogrammer <minhspammail@gmail.com>
2024-01-20 13:23:22 -0500

    PICKUP SCRIPT

--- Assets/Scripts/Player_movement.cs
+++ Assets/Scripts/Player_movement.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Player_movement : MonoBehaviour
+public class Player_Movement : MonoBehaviour
 {
     public CharacterController controller;
     public float walkSpeed = 12f;
@@ -15,9 +15,16 @@ public class Player_movement : MonoBehaviour
     public float groundDistance = 0.4f;
     public LayerMask groundMask;
 
+    public Transform playerCamera; // Assuming the camera is a child of the player object
+
+    public float objectOffset = 0.5f;
+    public Transform AttachPoint; // Reference to the player arm
+    private GameObject carriedObject;
+    public float pickupRange = 3f;
+
     Vector3 velocity;
     bool isGrounded;
-    
+
     void Update()
     {
         isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
@@ -42,8 +49,61 @@ public class Player_movement : MonoBehaviour
             velocity.y = Mathf.Sqrt(jumpHeight * -2f * gravity);
         }
 
+        // Object Pickup
+        if (Input.GetButtonDown("Fire2")) //Right Mouse Button
+        {
+            if (carriedObject == null)
+            {
+                TryPickupObject();
+            }
+            else
+            {
+                DropObject();
+            }
+        }
+
+        if (carriedObject != null && Input.GetButtonDown("Fire1")) //Left Mouse Button
+        {
+            TriggerEffect();
+        }
+
         velocity.y += gravity * Time.deltaTime;
 
         controller.Move(velocity * Time.deltaTime);
+    }
+
+    void TryPickupObject()
+    {
+        RaycastHit hit;
+        if (Physics.Raycast(playerCamera.position, playerCamera.forward, out hit, pickupRange))
+        {
+            if (hit.collider.CompareTag("Pickupable"))
+            {
+                carriedObject = hit.collider.gameObject;
+                carriedObject.GetComponent<Rigidbody>().isKinematic = true; // Disable physics
+
+                // Calculate the offset in the left direction relative to the camera
+                Vector3 offset = -playerCamera.up * objectOffset;
+
+                // Snap the object to the attach point position with the offset
+                carriedObject.transform.position = AttachPoint.position + offset;
+
+                // Attach the object to the player's arm
+                carriedObject.transform.parent = AttachPoint;
+            }
+        }
+    }
+
+    void DropObject()
+    {
+        carriedObject.GetComponent<Rigidbody>().isKinematic = false; // Enable physics
+        carriedObject.transform.parent = null; // Detach the object
+        carriedObject = null;
+    }
+
+    void TriggerEffect()
+    {
+        // Add your effect logic here
+        Debug.Log("Effect triggered!");
     }
 }
